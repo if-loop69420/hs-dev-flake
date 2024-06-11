032e2e4ffaaa631ba97c3fed3a5250f1e85298b8
if-loop69420 <j.sztavi@pm.me>
2024-06-11 11:17:39 +0200

    Smited flake-utils from the heavens

--- flake.nix
+++ flake.nix
@@ -1,18 +1,18 @@
 {
   description = "Flake i stole to use stack for haskell...";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs }: let
+    forAllSystems = function: 
+      nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+      ] (system: function nixpkgs.legacyPackages.${system});
+  in {
+    devShells = forAllSystems (pkgs:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        hPkgs = pkgs.haskell.packages."ghc981";
 
-        hPkgs =
-          pkgs.haskell.packages."ghc981"; # need to match Stackage LTS version
-                                           # from stack.yaml resolver
-
-        myDevTools = [
+        devTools = [
           hPkgs.ghc # GHC compiler in the desired version (will be available on PATH)
           hPkgs.ghcid # Continuous terminal Haskell compile checker
           hPkgs.ormolu # Haskell formatter
@@ -26,11 +26,6 @@
           stack-wrapped
         ];
 
-        # Wrap Stack to work with our Nix integration. We don't want to modify
-        # stack.yaml so non-Nix users don't notice anything.
-        # - no-nix: We don't want Stack's way of integrating Nix.
-        # --system-ghc    # Use the existing GHC on PATH (will come from this Nix file)
-        # --no-install-ghc  # Don't try to install GHC if no matching GHC found on PATH
         stack-wrapped = pkgs.symlinkJoin {
           name = "stack"; # will be available as the usual `stack` in terminal
           paths = [ pkgs.stack ];
@@ -44,13 +39,12 @@
           '';
         };
       in {
-        devShells.default = pkgs.mkShell {
-          buildInputs = myDevTools;
+        default = pkgs.mkShell {
+          buildInputs = devTools;
 
-          # Make external Nix c libraries like zlib known to GHC, like
-          # pkgs.haskell.lib.buildStackProject does
-          # https://github.com/NixOS/nixpkgs/blob/d64780ea0e22b5f61cd6012a456869c702a72f20/pkgs/development/haskell-modules/generic-stack-builder.nix#L38
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath myDevTools;
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath devTools;
         };
-      });
+      }
+    );
+  };
 }
